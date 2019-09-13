8bfb726a1a15c5da89317fed2c63570771c98d52
Seth <cheesefrycook00@gmail.com>
2019-09-13 17:07:33 -0500

    more movement and animations
    added in the animations for the player and better movement. also added a camera

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,60 +1,117 @@
 extends KinematicBody2D
 
 const UP = Vector2(0, -1)
-const GRAVITY = 40
+const GRAVITY = Vector2(0, 900)
 const MAX_FALL_SPEED = 500
-const JUMP_HEIGHT = 700
+const JUMP_SPEED = 400
+const JUMP_HEIGHT = 400
 const ACCELERATION = 50
-const MAX_SPEED = 300
+const AIR_ACCELERATION = 30
+const MAX_SPEED = 200
 const MAX_DASH_SPEED = 1500
 const AIR_FRICTION = 0.05
 const GROUND_FRICTION = .3
 const JUMP_FALLOFF_SPEED = .5
+const JUMP_BUFFER = 6
 
 var motion = Vector2()
 var friction = false
 
+var anim = ""
+var new_anim = ""
+var facing_right = true
+var falling = false
+var jump_buffer = 0
 
 func _physics_process(delta):
-#	if Input.is_action_pressed("ui_right"):
-#		motion.x = SPEED
-#	elif Input.is_action_pressed("ui_left"):
-#		motion.x = -SPEED
-#	else:
-#		motion.x = 0
-#
-#	if is_on_floor():
-#		if Input.is_action_just_pressed("ui_up"):
-#			motion.y = JUMP_VEL
-#
-#	motion.y += GRAVITY
-#
-#	motion = move_and_slide(motion, UP)
 
+	#add gravity
+	motion += delta * GRAVITY
 	
-	if motion.y < MAX_FALL_SPEED:
-		motion.y += GRAVITY
+	#move
+	motion = move_and_slide(motion, UP)
+	
+	if anim != "squash":
+		new_anim = "idle"
+	
+	#play landing animation when hitting ground
+	if is_on_floor() and anim == "jump_fall":
+		new_anim = "squash"
 
-	if is_on_floor():
-		if Input.is_action_just_pressed("jump"):
-			motion.y = -JUMP_HEIGHT
+	if Input.is_action_just_pressed("jump"):
+		jump_buffer=JUMP_BUFFER
+	elif jump_buffer >= 0:
+		jump_buffer-=1
+	
+
+	#if we're on the floor and press jump, then jump
+	if (is_on_floor() or jump_buffer > 0) and Input.is_action_just_pressed("jump"):
+		motion.y = -JUMP_SPEED
+		jump_buffer = 0
+		new_anim = "jump_inital"
+	#play animations in air
+	elif !is_on_floor():
+		if motion.y > -100 and motion.y < 100:
+			new_anim = "jump_peak"
+		elif motion.y < 0 and motion.y > -JUMP_SPEED+100:
+			new_anim = "jump_rise"
+		elif motion.y > 0:
+			new_anim = "jump_fall"
+			falling = true
+	
+	#if we release the jump key while rising then cut off the jump
 	if Input.is_action_just_released("jump") and motion.y < 0:
 		motion.y = lerp(motion.y, 0, JUMP_FALLOFF_SPEED)
+		new_anim = "jump_peak"
 			
-	friction = false
-
-	if Input.is_action_pressed("move_right"):
-		motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
-	elif Input.is_action_pressed("move_left"):
-		motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
+	#movement on ground
+	if is_on_floor():
+		if Input.is_action_pressed("move_right"):
+			motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
+			if motion.x < ACCELERATION*4:
+				new_anim = "run_inital"
+			else:
+				new_anim = "run"
+			facing_right = true
+			friction = false
+		elif Input.is_action_pressed("move_left"):
+			motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
+			if motion.x > ACCELERATION*4:
+				new_anim = "run_inital"
+			else:
+				new_anim = "run"
+			facing_right = false
+			friction = false
+		else:
+			friction = true
+	#movement in air
 	else:
-		friction = true
+		if Input.is_action_pressed("move_right"):
+			motion.x = min(motion.x+AIR_ACCELERATION, MAX_SPEED)
+			facing_right = true
+			friction = false
+		elif Input.is_action_pressed("move_left"):
+			motion.x = max(motion.x-AIR_ACCELERATION, -MAX_SPEED)
+			facing_right = false
+			friction = false
+		else:
+			friction = true
 
+	#deceleration
 	if is_on_floor():
 		if friction:
 			motion.x = lerp(motion.x, 0, GROUND_FRICTION)
 	else:
 		if friction:
 			motion.x = lerp(motion.x, 0, AIR_FRICTION)
-		
-	motion = move_and_slide(motion, UP)
+
+func _process(delta):
+	#play animation
+	if anim != new_anim:
+		anim = new_anim
+		$Sprite.play(anim)
+	$Sprite.flip_h = !facing_right
+
+func _on_Sprite_animation_finished():
+	if $Sprite.animation == "squash":
+		new_anim = "idle"
