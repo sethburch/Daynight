3ef75fd309830c82ae0d27ddc7f63472640eb36f
Seth <cheesefrycook00@gmail.com>
2019-10-21 16:45:22 -0500

    parallax and camera stuff
    added in a parallax background and the camera works a bit different now

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -45,6 +45,8 @@ func _ready():
 	add_to_group("Player")
 
 func _physics_process(delta):
+	#$Camera2D.zoom = Global.viewport.size/Global.window_size
+	
 	haxis = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	vaxis = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	
