50a80e595dc91a9f75995afb00453e90f2d433e9
Seth <cheesefrycook00@gmail.com>
2019-11-14 09:24:33 -0600

    snow and rain

--- scripts/World.gd
+++ scripts/World.gd
@@ -2,10 +2,23 @@ extends Node
 
 func _ready():
 	randomize()
+	for i in range(-100, 100):
+		for j in range(-100, 100):
+			if $TileMap.get_cell(i, j) != TileMap.INVALID_CELL:
+				#$RainSplash.set_cell((i*4), (j*4)-3, 0)
+				break
+			#$Rain.set_cell(i, j, 0)
+			$SnowBottom.set_cell(i, j, 0)
+			$SnowTop.set_cell(i, j, 0)
 	
 func _process(delta):
 	if Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
+	if Input.is_action_just_pressed("scroll_down"):
+		$TileMap.tile_set.tile_set_texture(1, load("res://sprites/snow_grass.png"))
+	if Input.is_action_just_pressed("scroll_up"):
+		$TileMap.tile_set.tile_set_texture(1, load("res://sprites/grass_tileset5.png"))
+		
 		
 func screen_shake(duration, frequency, amplitude):
 	get_node("Player/Camera2D").shake(duration, frequency, amplitude)
